a6bacece319c8d88b53d5541438d99ef8b3897da
KirimLee <kirim@sk.com>
2018-12-14 08:59:56 +0900

    탑승요청 반환을 한사람으로만 제한하기로 함

--- README.md
+++ README.md
@@ -76,7 +76,7 @@ src 서버에 들어가서 nodemon으로 ts-node를 이용해서 index.ts를 실
 - [x] 근처 운전자 보기
 - [x] Subscribe 으로 근처 운전자 보기
 - [x] 탑승요청
-- [ ] 근처 탑승요청 받기
+- [x] 근처 탑승요청 받기
 - [ ] Subscribe to Nearby Ride Requests
 - [ ] Subscribe to Ride Status
 - [ ] 채팅방 메세지 받기
--- src/api/Ride/GetNearByRide/GetNearByRide.resolvers.ts
+++ src/api/Ride/GetNearByRide/GetNearByRide.resolvers.ts
@@ -1,39 +1,47 @@
 import { Resolvers } from "../../../types/resolvers";
 import authResolver from "../../../utils/resolverMiddleware";
-import { GetNearByRidesResponse } from "../../../types/graph";
+import { GetNearByRideResponse } from "../../../types/graph";
 import { getRepository, Between } from "typeorm";
 import User from "../../../entities/User";
 import Ride from "../../../entities/Ride";
 
 const resolvers: Resolvers = {
     Query: {
-        GetNearByRides: authResolver(async(_, __, { req }): Promise<GetNearByRidesResponse> => {
+        GetNearByRide: authResolver(async(_, __, { req }): Promise<GetNearByRideResponse> => {
             const user: User = req.user;
             const { lastLat, lastLng } = user;
             if(user.isDriving) {
                 try{
-                    const rides = await getRepository(Ride).find({
+                    const ride = await getRepository(Ride).findOne({
                         status: "REQUESTING",
                         pickUpLat: Between(lastLat - 0.05, lastLat + 0.05),
                         pickUpLng: Between(lastLng - 0.05, lastLng + 0.05)
                     });
-                    return {
-                        ok: true,
-                        error: null,
-                        rides
-                    }
+                    if (ride) {
+                        return {
+                            ok: true,
+                            error: null,
+                            ride
+                        }
+                    } else {
+                        return {
+                            ok: true,
+                            error: null,
+                            ride: null
+                        }
+                    }                    
                 }catch(error) {
                     return {
                         ok: false,
                         error: error.message,
-                        rides: null                  
+                        ride: null                  
                     }
                 }
             } else {
                 return {
                     ok: false,
                     error: "You are not a driver",
-                    rides: null
+                    ride: null
                 }
             }
             
